1b2b24a52c6ed1537a7b8223ce45c6e424785b38
Nicolas Baraldo <121053856+NicolasBaraldodeLima@users.noreply.github.com>
2023-09-26 16:44:23 -0300

    Add files via upload

---
+++ operador-ternario.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	
+	
+	
+	
+	 int n1, n2, nota;
+	 string res;
+	 
+	 cout << "Digite a primeira nota:";
+	 cin >> n1;
+	 cout << "Digite a segunda nota:";
+	 cin >> n2;
+	 
+	 nota = n1 + n2;
+	 
+	 
+	 (nota >= 60) ? res = "Aprovado" : res = "Reprovado";
+	 
+	 cout << "n\Situação do aluno: " << res << "\n";
+	 
+	 return 0;
+	
+	
+}
